8b5288add0f4307fa35924adea12a132acbeab4e
Thomas Peek <peekthomas369@icloud.com>
2023-02-14 14:49:33 -0700

    HousesSchema Imported in DB folder

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
+import { HouseSchema } from '../models/Houses';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  Houses = mongoose.model('Houses', HousesSchema);
 }
 
 export const dbContext = new DbContext()
