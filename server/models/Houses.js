bed8dd70049e696fcc99418cc4352f9a826e40bb
Thomas Peek <peekthomas369@icloud.com>
2023-02-14 14:46:42 -0700

    house Model built out

---
+++ server/models/Houses.js
@@ -0,0 +1,12 @@
+import { Schema } from "mongoose"; 
+
+export const HouseSchema = new Schema(
+    {
+        bedrooms: {type: Number, required: true, min: 1, max: 100},
+        bathrooms: {type: Number, required: true, min: 1, max: 100},
+        year: {type: Number, required: true, min: 1, max:5000},
+        imgUrl: {type: String, required: true, maxLength: 5000, required: true},
+        description: {type: String, required: true, minLength: 10, maxLength: 5000},
+    },
+    {timestamps: true, toJSON: { virtuals: true }}
+)
